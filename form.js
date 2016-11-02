72b06b4b7c7402b2cf96731b52e8aabe3cdf4d80
mitchrickman14four <mitch@14four.com>
2016-11-02 15:44:13 -0700

    add window load event

--- form.js
+++ form.js
@@ -24,45 +24,46 @@
     }
     return "";
   }
+  window.onload = function() {
+    var form = document.getElementById('mainform123');
+    var input = document.getElementById('id123-control23176353');
+    var button = document.getElementById('id123-button-send');
+    var clickHandler;
+    var cookieName = 'usedEmails';
+    var tempCookieName = 'tempEmail';
+    var cookieManager = new CookieManager();
 
-  var form = document.getElementById('mainform123');
-  var input = document.getElementById('id123-control23176353');
-  var button = document.getElementById('id123-button-send');
-  var clickHandler;
-  var cookieName = 'usedEmails';
-  var tempCookieName = 'tempEmail';
-  var cookieManager = new CookieManager();
+    var isEmailUnique = function( emailAddress ) {
+      var cookieValue = cookieManager.get(cookieName);
+      return cookieValue.indexOf(emailAddress) > -1 ? false : true;
+    };
 
-  var isEmailUnique = function( emailAddress ) {
-    var cookieValue = cookieManager.get(cookieName);
-    return cookieValue.indexOf(emailAddress) > -1 ? false : true;
-  };
-
-  var appendToCookie = function ( emailAddress ) {
-    var cookieValue = cookieManager.get(cookieName);
-    if ( cookieValue == "" ) {
-      cookieManager.set(cookieName, emailAddress);
+    var appendToCookie = function ( emailAddress ) {
+      var cookieValue = cookieManager.get(cookieName);
+      if ( cookieValue == "" ) {
+        cookieManager.set(cookieName, emailAddress);
+      }
+      else {
+        cookieManager.set(cookieName, cookieValue + "," + emailAddress);
+      }
     }
-    else {
-      cookieManager.set(cookieName, cookieValue + "," + emailAddress);
-    }
+
+    var onFormSubmit = function( event ) {
+      var isUnique = isEmailUnique( input.value );
+      clickHandler = typeof clickHandler == 'undefined' ? button.onclick : clickHandler;
+      button.onclick = false
+
+      if ( isUnique ) {
+        cookieManager.set(tempCookieName, input.value);
+        clickHandler.call(this, event);
+      }
+      else {
+        event.preventDefault();
+        event.stopPropagation();
+        alert('You’ve already entered today. Check back tomorrow and get another cast.');
+      }
+    };
+
+    button.addEventListener('click', onFormSubmit);
   }
-
-  var onFormSubmit = function( event ) {
-    var isUnique = isEmailUnique( input.value );
-    clickHandler = typeof clickHandler == 'undefined' ? button.onclick : clickHandler;
-    button.onclick = false
-
-    if ( isUnique ) {
-      cookieManager.set(tempCookieName, input.value);
-      clickHandler.call(this, event);
-    }
-    else {
-      event.preventDefault();
-      event.stopPropagation();
-      alert('You’ve already entered today. Check back tomorrow and get another cast.');
-    }
-  };
-
-  button.addEventListener('click', onFormSubmit);
 })();
