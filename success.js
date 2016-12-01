c67ab8386db5abcdb9bce5d079c935147896b445
mitchrickman14four <mitch@14four.com>
2016-12-01 12:18:52 -0800

    set timeout

--- success.js
+++ success.js
@@ -54,30 +54,33 @@
   function trackConversion() {
     var onScriptLoad = function() {
       console.log('script loaded');
-      /* <![CDATA[ */
-      var goog_snippet_vars = function() {
-        var w = window;
-        w.google_conversion_id = 869038450;
-        w.google_conversion_label = "xJPnCJjri2wQ8vKxngM";
-        w.google_remarketing_only = false;
-      }
-      // DO NOT CHANGE THE CODE BELOW.
-      goog_report_conversion = function(url) {
-        goog_snippet_vars();
-        window.google_conversion_format = "3";
-        var opt = new Object();
-        opt.onload_callback = function() {
-          if (typeof(url) != 'undefined') {
-            window.location = url;
+      setTimeout(function() {
+        /* <![CDATA[ */
+        goog_snippet_vars = function() {
+          var w = window;
+          w.google_conversion_id = 869038450;
+          w.google_conversion_label = "xJPnCJjri2wQ8vKxngM";
+          w.google_remarketing_only = false;
+        }
+        // DO NOT CHANGE THE CODE BELOW.
+        goog_report_conversion = function(url) {
+          goog_snippet_vars();
+          window.google_conversion_format = "3";
+          var opt = new Object();
+          opt.onload_callback = function() {
+            if (typeof(url) != 'undefined') {
+              window.location = url;
+            }
+          }
+          var conv_handler = window['google_trackConversion'];
+          if (typeof(conv_handler) == 'function') {
+            conv_handler(opt);
           }
         }
-        var conv_handler = window['google_trackConversion'];
-        if (typeof(conv_handler) == 'function') {
-          conv_handler(opt);
-        }
-      }
-      /* ]]> */
-      console.log('conversion tracked');
+        /* ]]> */
+        console.log('conversion tracked');
+        goog_report_conversion();
+      }, 2000);
     };
 
     var script = document.createElement('script');
