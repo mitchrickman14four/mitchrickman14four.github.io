611dd569c2fb2ed8c780903880c69e0b3f038dfa
mitchrickman14four <mitch@14four.com>
2016-12-01 12:01:27 -0800

    add tracking script

--- success.js
+++ success.js
@@ -51,5 +51,46 @@
 
   addTempCookie();
 
-  
+  function trackConversion() {
+    var onScriptLoad = function() {
+      console.log('script loaded');
+      /* <![CDATA[ */
+      goog_snippet_vars = function() {
+        var w = window;
+        w.google_conversion_id = 869038450;
+        w.google_conversion_label = "xJPnCJjri2wQ8vKxngM";
+        w.google_remarketing_only = false;
+      }
+      // DO NOT CHANGE THE CODE BELOW.
+      goog_report_conversion = function(url) {
+        goog_snippet_vars();
+        window.google_conversion_format = "3";
+        var opt = new Object();
+        opt.onload_callback = function() {
+          if (typeof(url) != 'undefined') {
+            window.location = url;
+          }
+        }
+        var conv_handler = window['google_trackConversion'];
+        if (typeof(conv_handler) == 'function') {
+          conv_handler(opt);
+        }
+      }
+      /* ]]> */
+      console.log('conversion tracked');
+    };
+
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.addEventListener('load', onScriptLoad);
+
+    // add script to page
+    document.querySelector('body').appendChild( script );
+
+    // load script
+    script.src = '//www.googleadservices.com/pagead/conversion_async.js';
+  }
+
+  trackConversion();
+
 })();
