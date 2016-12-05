8706d845d76f63233ba6e40c38144d52cd82f19e
Dustin Dikes <dustindikes@gmail.com>
2016-12-05 14:32:54 -0800

    Moved tracking to form instead of success

--- success.js
+++ success.js
@@ -51,48 +51,4 @@
 
   addTempCookie();
 
-  function addTrackingScript() {
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-
-    document.querySelector('body').appendChild( script );
-    script.innerHTML = "/* <![CDATA[ */ \
-    goog_snippet_vars = function() { \
-    var w = window; \
-    w.google_conversion_id = 869038450; \
-    w.google_conversion_label = 'xJPnCJjri2wQ8vKxngM'; \
-    w.google_remarketing_only = false; \
-    }; \
-    goog_report_conversion = function(url) { \
-    goog_snippet_vars(); \
-    window.google_conversion_format = '3'; \
-    var opt = new Object(); \
-    opt.onload_callback = function() { \
-    if (typeof(url) != 'undefined') { \
-    window.location = url; \
-    } \
-    }; \
-    var conv_handler = window['google_trackConversion']; \
-    if (typeof(conv_handler) == 'function') { \
-    conv_handler(opt); \
-    } \
-    }; \
-    goog_report_conversion(); \
-    /* ]]> */";
-  }
-
-  function trackConversion() {
-    addTrackingScript();
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-
-    // add script to page
-    document.querySelector('body').appendChild( script );
-
-    // load script
-    script.src = '//www.googleadservices.com/pagead/conversion_async.js';
-  }
-
-  trackConversion();
-
 })();
