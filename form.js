aeb6d9181694a2cac5d28df182b4ae7356fd842b
Dustin Dikes <dustindikes@gmail.com>
2016-12-05 14:46:22 -0800

    Added timeout for form submission event dispatch

--- form.js
+++ form.js
@@ -97,9 +97,22 @@
     var isUnique = isEmailUnique( input.value );
 
     if ( isUnique ) {
+      // Get the target event
+      var t = event.target;
+
+      // Temporarily stop propagation
+      event.preventDefault();
+      event.stopPropagation();
+
+      // Track conversion, set cookie, call click handler
       trackConversion();
       cookieManager.set(tempCookieName, input.value);
       clickHandler.call(this, event);
+
+      // Timeout to allow tracking to take place
+      setTimout(function(){
+        t.dispatchEvent(event);
+      },1000);
     }
     else {
       event.preventDefault();
